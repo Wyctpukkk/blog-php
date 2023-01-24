4abd2e6aa7976f963a611a37dcab6616b76477c9
Vitaly <poroshenkoe@gmail.com>
2023-01-25 00:00:03 +0300

    add includes, add header component, refactoring

---
+++ includes/config.php
@@ -0,0 +1,16 @@
+<?php 
+
+$config = array (
+    'title' => 'Блог Джуна',
+    'vk_url' => 'https://vk.com/',
+    'db' => array(
+        'server' => 'localhost',
+        'username' => 'root',
+        'password' => '',
+        'name' => 'test_blog'
+    )
+    );
+
+require "db.php"
+
+?>
